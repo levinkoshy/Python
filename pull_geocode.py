84ba5be6d7aecf21b7c165c17f7c8711a10377f3
levinkoshy <levinkoshy@users.github.com>
2018-01-27 12:08:54 -0600

    python snippets
    to add geocode to location field

---
+++ pull_geocode.py
@@ -0,0 +1,16 @@
+import csv
+import geocoder
+from tqdm import tqdm
+
+outfile = open("withgeocode.csv","w")
+
+with open(r"C:\Users\levi3\OneDrive\Documents\DataChallenge_SayariAnalytics\csv_bahamas_leaks.2017-12-19\bahamas_leaks.nodes.address.csv","r") as csvfile:
+    filer = csv.DictReader(csvfile)
+    outfieldnames = filer.fieldnames.append("geocode")
+    print(filer.fieldnames)
+    writer = csv.DictWriter(outfile,fieldnames = filer.fieldnames)
+    writer.writeheader()
+    for each in tqdm(filer):
+        g = geocoder.google(each["address"]) 
+        each["geocode"] = g.latlng 
+        writer.writerow(each)
